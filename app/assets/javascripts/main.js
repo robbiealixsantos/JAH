49298cd6b3f0abc4e6d00c7c6a92bffd51348943
Robbie Santos <robbiesantos@Robbies-MacBook-Pro.local>
2017-01-17 18:55:01 -0800

    complete initial mvp of JAH main page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,10 +1,29 @@
 $(document).ready(function(){
 
-    $("video").prop('muted', true);
+    $("video").prop('muted', false);
     $("video").prop("volume", 0.1);
 
     $("video").click( function (){
-    $(this).prop('muted', !$(this).prop('muted'));
+    $(this).prop('muted', !$(this).prop('muted'), !$(this).prop("volume", 0.1) );
     });
+
+    $('div.dropdown').each(function() {
+    var $dropdown = $(this);
+
+    $("a.dropdown-link", $dropdown).click(function(e) {
+      e.preventDefault();
+      $div = $("div.dropdown-container", $dropdown);
+      $div.toggle();
+      $("div.dropdown-container").not($div).hide();
+        return false;
+      });
+
+    });
+
+
+  $('#contact-link').on("click", function(event){
+    event.preventDefault();
+    $('#contact').toggle();
+  });
 
 });
